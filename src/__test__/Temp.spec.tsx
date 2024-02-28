e39ae732e5bd5b18b0b8d31b68657987cb8352c3
shin <sinej1020@gmail.com>
2024-02-28 22:12:51 +0900

    setting

---
+++ src/__test__/Temp.spec.tsx
@@ -0,0 +1,11 @@
+describe('it, test의 묶음', () => {
+    beforeEach(() => {
+        console.log("beforeEach")
+    })
+    it('should test the first case', () => {
+        console.log("it")
+    });
+    test('테스트케이스 1번', () => {
+        console.log("test")
+    });
+});
